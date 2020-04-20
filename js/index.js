ab88549a78b630bb5766f1812d04e036b9e8a030
manish-dci <manishwild1000@gmail.com>
2020-04-20 15:10:35 +0200

    started-projecet

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.onload = ()=>{
+    //getting html events
+    const canvas = document.querySelector('#canavas')
+    //create context from canvas
+    const ctx = canvas.getContext('2d')
+    ctx.moveTo(0,250)
+
+}
